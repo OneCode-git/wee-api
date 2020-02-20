bc2672d01e89c8f0c27a041642a93ec32c213d34
ChaitanyaBudapaneti <chaitanyabudapaneti@gmail.com>
2020-02-20 06:52:23 +0530

    added size and format validation to create shortLink.

--- src/main/java/com/wee/controller/UrlController.java
+++ src/main/java/com/wee/controller/UrlController.java
@@ -8,6 +8,8 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.wee.entity.Url;
 import com.wee.service.UrlClickService;
 import com.wee.service.UrlService;
+import com.wee.util.Commons;
 
 /**
  * @author chaitu
@@ -48,9 +51,14 @@ public class UrlController {
 	}
 	
 	@PostMapping(path= "", consumes = "application/json", produces = "text/plain")
-	String create(@RequestBody Url url) {
-		return urlService.create(url);
-		
+	ResponseEntity<String> create(@RequestBody Url url) {
+		if(Commons.isValidURL(url.getOriginalUrl())) {
+			if(url.getOriginalUrl().length() > 2000)
+				return new ResponseEntity<String>("max length exceeded", HttpStatus.BAD_REQUEST);
+			String shortURL = urlService.create(url);
+			return new ResponseEntity<String>(shortURL, HttpStatus.CREATED);
+		}
+		return new ResponseEntity<String>("invalid URL", HttpStatus.BAD_REQUEST);		
 	}
 	
 }
