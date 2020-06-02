f75eb87e129485353fcadaad8382cdd7cb0b4b38
jayalakshmi <you@example.com>
2020-06-02 14:45:34 +0530

    resolved crossorigin issue

--- src/main/java/com/wee/controller/UrlController.java
+++ src/main/java/com/wee/controller/UrlController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +31,8 @@ public class UrlController {
 	
 	@Autowired UrlService urlService;
 	@Autowired UrlClickService urlClickService;
+	
+	
 	@GetMapping("{hash}")
 	void redirect(@PathVariable("hash") String hash, HttpServletResponse httpServletResponse,@RequestHeader("User-Agent") String userAgent) {
 		Optional<Url> oUrl = urlService.findByHash(hash);
@@ -46,7 +49,7 @@ public class UrlController {
 		return urlService.findByHash(hash);
 		
 	}
-	
+	@CrossOrigin(origins = "http://localhost:3000")
 	@PostMapping(path= "", consumes = "application/json", produces = "text/plain")
 	String create(@RequestBody Url url) {
 		return urlService.create(url);
