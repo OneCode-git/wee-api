c3d5053c8306702382dd0c30ed34275ee3925e16
Sajan Madaan <104887030+sajanmadaan1410@users.noreply.github.com>
2023-06-07 12:40:41 +0530

    Revert "Link click tracking enhancements"

--- src/main/java/com/wee/mybatis/mapper/UrlMapper.java
+++ src/main/java/com/wee/mybatis/mapper/UrlMapper.java
@@ -1,12 +1,12 @@
 /**
- *
+ * 
  */
 package com.wee.mybatis.mapper;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import com.wee.entity.UrlClick;
+
 import com.wee.entity.Url;
 
 /**
@@ -17,11 +17,7 @@ import com.wee.entity.Url;
 public interface UrlMapper {
 	@Insert("insert into url (hash, original_url) values(#{hash}, #{originalUrl})")
 	void save(Url url);
-
+	
 	@Select("select hash, original_url from url where hash = #{hash}")
 	Url findById(String hash);
-
-	@Insert("INSERT INTO url_click (user_ip, url_id, browser, browser_type, browser_major_version, device_type, platform, platform_version, created_ts) VALUES (#{userIp}, #{urlId}, #{browser}, #{browserType}, #{browserMajorversion}, #{deviceType}, #{platform}, #{platformVersion}, #{createdTs})")
-	void saveInUrlClick(UrlClick urlClick);
-
 }
