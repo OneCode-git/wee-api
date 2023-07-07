132bc7dda283e137960b0af574112b9a72ce2748
bhaskaras1999 <128044930+bhaskaras1999@users.noreply.github.com>
2023-07-07 12:43:55 +0530

    Staging3 (#38)
    * restart
    
    * removed breaking code
    
    * reverted code back to master
    
    * reveted code back to master

--- src/main/java/com/wee/service/UrlServiceImpl.java
+++ src/main/java/com/wee/service/UrlServiceImpl.java
@@ -3,15 +3,8 @@
  */
 package com.wee.service;
 
-import java.sql.Timestamp;
-import java.util.Date;
-import java.util.Map;
 import java.util.Optional;
 
-import netscape.javascript.JSObject;
-import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -21,8 +14,6 @@ import com.wee.entity.Url;
 import com.wee.mybatis.mapper.UrlMapper;
 import com.wee.repo.UrlRepo;
 import com.wee.util.Commons;
-import java.sql.Timestamp;
-import java.util.Date;
 
 /**
  * @author chaitu
@@ -30,7 +21,7 @@ import java.util.Date;
  */
 @Service
 public class UrlServiceImpl implements UrlService{
-	private static final Logger logger = LoggerFactory.getLogger(UrlClickServiceImpl.class);
+
 	@Autowired
 	UrlRepo urlRepo;
 	@Autowired UrlMapper urlMapper;
@@ -49,54 +40,23 @@ public class UrlServiceImpl implements UrlService{
 	 * @see com.wee.service.UrlService#create(com.wee.entity.Url)
 	 */
 	@Override
-	public String create(Url url, String metadata) {
-		String hash = generateTinyUrl(url,metadata);
+	public String create(Url url) {
+		String hash = generateTinyUrl(url);
 		if (url.getGenClickId() != null && url.getGenClickId() == true) {
 			return weeBaseUrl+ "c/" + hash;
 		}
 		return weeBaseUrl+hash;
 	}
-
-	String convertIntoJsonString(String metaData){
-
-
-		// Replace equal sign with colon
-		String jsonString = metaData.replaceAll("=", ":");
-
-		// Create a JSONObject from the string
-		JSONObject jsonObject = new JSONObject(jsonString);
-
-		// Convert the JSONObject to a JSON string
-		String newMetaData = jsonObject.toString();
-		return newMetaData;
-	}
 	
-	String generateTinyUrl(Url url , String metaData) {
+	String generateTinyUrl(Url url) {
 		String hash = Commons.genHash(url.getOriginalUrl());
-		Timestamp created_at  = new Timestamp(System.currentTimeMillis());
-		if (isCollisionDetected(hash)){
-			generateTinyUrl(url,metaData);
-		}
-		String jsonMetaData = convertIntoJsonString(metaData);
 		url.setHash(hash);
-		url.setCreatedTs(created_at);
-		url.setMetadata(jsonMetaData);
 		try {
 			urlRepo.save(url);
-			logger.info("Saved tiny url for url: "+url+" successfully");
 		}catch (DataIntegrityViolationException e) {
-			logger.error("Failed to save tiny url for url: "+url);
-			return generateTinyUrl(url,metaData);
+			return generateTinyUrl(url);
 		}
 		return hash;
-	}
-
-	public  boolean isCollisionDetected(String hash) {
-		Optional<Url> url = urlRepo.findById(hash);
-		if (url.isPresent()) {
-			return true;
-		}
-		return false;
 	}
 
 }
