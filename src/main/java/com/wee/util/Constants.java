50e87bdfe0d16886ea8df87652b2f77acd612838
bhaskaras1999 <bhaskara.s@zetapp.in>
2025-05-30 12:30:27 +0530

    rsolved link issue

--- src/main/java/com/wee/util/Constants.java
+++ src/main/java/com/wee/util/Constants.java
@@ -1,8 +1,12 @@
 package com.wee.util;
 
+import java.util.List;
+
 public class Constants {
 
     public static final String REDIS_HASH_KEY = "redis.hash.";
     public static final String REDIS_URL_CLICK = "redis.url.click.";
     public static final String REDIRECTION_PATH = "/redirection/";
+
+    public static final String ZET_PROFILES = "prod-b2c";
 }
